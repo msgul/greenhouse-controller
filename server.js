7d6fa04298bb17f5d192a5cdcb37d12c37c4977f
Mert Şamil Gül <mertsamilgul@gmail.com>
2021-01-07 17:10:19 +0300

    temp input added

--- server.js
+++ server.js
@@ -11,21 +11,43 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-const gh_list = {}
+const gh_list = [];
+const gh_temp = [];
+//const offline = [];
 
 io.on('connection', function (socket) {
+
+    /*
+    socket.on('disconnect', function() {
+        offline.push(id);
+     });
+    */
 
     setInterval(() => {
 
         for(i in gh_list)
             socket.emit('gh_info',i,gh_list[i]);
+        
+        /*
+        for(i in offline)
+            socket.emit('gh_offline',offline[i]);
+        */
 
-    }, 3000);
+    }, 1000);
 
-    socket.on('gh_info',(gh_id,temp) => {
-        gh_list[gh_id] = temp; // update greenhouse temp
+    socket.on('gh_temp',(id,temp) => {
+        console.log("New temp: " + id + "," + temp);
+        gh_temp[id] = temp;
     });
 
+    socket.on('gh_info',(id,temp) => {
+        gh_list[id] = temp; // update greenhouse temp
+
+        if(gh_temp[id]){
+            socket.emit('gh_temp',gh_temp[id])
+            delete gh_temp[id];
+        }
+    });
 });
 
 http.listen(port);
