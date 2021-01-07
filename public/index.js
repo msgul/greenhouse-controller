eb664ae639eb1c4af13de3c84c9a833a5d299ede
Mert Şamil Gül <mertsamilgul@gmail.com>
2021-01-07 17:11:38 +0300

    temp input added

--- public/index.js
+++ public/index.js
@@ -7,6 +7,16 @@ var maxData = 10;
 
 const list = document.getElementById('gh-list');
 
+const clicks = (id) => {
+    
+    temp = document.getElementById(id + "i").value;
+    console.log(temp);
+    
+    socket.emit('gh_temp',id,temp);
+
+}
+
+
 const update = (id,temp) => {
 
     if(!gh_labels[id]){
@@ -16,9 +26,21 @@ const update = (id,temp) => {
 
         // creating new label for greenhouse
         gh_labels[id] = document.createElement('a');
-        gh_labels[id].innerHTML = 'ID: ' + id+ ' <span>' + temp + " °C</span>";
         big_div.appendChild(gh_labels[id]);
         
+
+        const form_div = document.createElement('div');
+        form_div.className = "form-div";
+        const input = document.createElement('input');
+        input.id = id + "i";
+        const button_span = document.createElement('span');
+        button_span.innerHTML = "<button onclick=\"clicks("+ id + ")\">Değiştir</button>"
+        
+        form_div.appendChild(input);
+        form_div.appendChild(button_span);
+
+        big_div.appendChild(form_div);
+
         // creating new graph for greenhouse
         dps[id] = [];
         tempIndex[id] = 2;
@@ -27,33 +49,29 @@ const update = (id,temp) => {
 
         const graph_div = document.createElement('div');
         graph_div.id = id;
+        graph_div.className = "graph";
         big_div.appendChild(graph_div);
-        big_div.appendChild(document.createElement("br"));
-        big_div.appendChild(document.createElement("br"));
-        big_div.appendChild(document.createElement("br"));
-        big_div.appendChild(document.createElement("br"));
-        big_div.appendChild(document.createElement("br"));
-        big_div.appendChild(document.createElement("br"));
 
         charts[id] = new CanvasJS.Chart(id, {
             title :{text: ""},
             data: [{type: "line",dataPoints: dps[id]}]
         });
-        charts[id].render();
 
     }
     else
     {
-        gh_labels[id].innerHTML = 'ID: ' + id+ ' <span>' + temp + " °C</span>";
         dps[id].push({x:tempIndex[id],y:temp});
         tempIndex[id]++;
 
         if(dps[id].length > maxData)
             dps[id].shift();
-
-        charts[id].render();
     } 
+
+    gh_labels[id].innerHTML = 'ID: ' + id+ ' <span>' + temp + " °C</span>";
+    charts[id].render();
 };
+
+
 
 const socket = io();
 
